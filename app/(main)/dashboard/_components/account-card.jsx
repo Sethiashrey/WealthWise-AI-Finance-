998dbea5eaad7522e668fb0ea31a187bd55a43fc
Shrey Sethia <shreysethia4810@gmail.com>
2025-10-01 22:49:57 +0530

    Initial Commit

---
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -0,0 +1,42 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Switch } from '@/components/ui/switch';
+import Link from 'next/link';
+import { parse } from 'date-fns/parse';
+import { ArrowDownRight, ArrowUpRight} from 'lucide-react';
+import React from 'react'
+
+const AccountCard = ({ account }) => {                       
+                                                                  
+    const { name,type, balance, id, isDefault } = account;
+    return (
+        <Card>
+            <Link href={`/account/${id}`}>
+
+            <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <Switch />
+            </CardHeader>
+            <CardContent>
+                <div className='text-2xl font-bold'>
+                    ₹{parseFloat(balance).toLocaleString('en-IN', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+                </div>
+                <p className='text-xs text-muted-foreground'>
+                    {type.charAt(0) + type.slice(1).toLowerCase()} Account
+                </p>
+            </CardContent>
+            <CardFooter>
+                <div className='flex items-center'>
+                    <ArrowUpRight className="mr-1 h-4 w-4 text-green-500" />
+                    Income
+                </div>
+                <div className='flex items-center'>
+                    <ArrowDownRight className="mr-1 h-4 w-4 text-red-500" />
+                    Expense
+                </div>
+            </CardFooter>
+            </Link>
+        </Card>
+    )
+}
+
+export default AccountCard
---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,23 @@
+import DashboardPage from "./page";
+import React from "react";
+import { BarLoader } from "react-spinners";
+import { Suspense } from "react";
+
+const DashBoard = () => {
+  return (
+    <div className="px-5">
+      
+        <h1 className="text-6xl font-bold gradient-title mb-5">
+          Dashboard
+        </h1>
+        
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <DashboardPage />
+      </Suspense>
+    </div>
+  );
+}
+
+export default DashBoard;
